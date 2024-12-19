e8c4b93ea86ccb8a6eaeb55339b67be341039f3a
Marzpy <marcinloin09@gmail.com>
2024-12-19 21:33:15 +0100

    c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -251,8 +251,6 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
 
 
 
-
-
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
     if (GPIO_Pin == PIR_Pin) {  // Jeśli przerwanie pochodzi od czujnika PIR
         if (HAL_GPIO_ReadPin(PIR_GPIO_Port, PIR_Pin) == GPIO_PIN_SET) {
